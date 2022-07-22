1f43f859fde4d96b4670ef44bb0ec992f7a71fd9
naoyukis <mitaka.214@gmail.com>
2022-07-22 11:10:04 +0900

    close files

--- main.go
+++ main.go
@@ -7,16 +7,25 @@ import (
 	"os"
 )
 
+func open(arg string) error {
+	f, err := os.Open(arg)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(os.Stdout, f); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	flag.Parse()
+
 	if flag.NArg() > 0 {
 		for _, arg := range flag.Args() {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				continue
-			}
-			if _, err := io.Copy(os.Stdout, f); err != nil {
+			if err := open(arg); err != nil {
 				fmt.Fprintln(os.Stderr, err)
 			}
 		}
